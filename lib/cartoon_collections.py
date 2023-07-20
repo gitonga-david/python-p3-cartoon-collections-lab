df5784a60c7ca3180006a48bbb5e194961c55bd2
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2023-07-20 21:09:14 +0300

    complete

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,17 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarfs):
+    [print(f"{dwarfs.index(dwarf) + 1}. {dwarf}") for dwarf in dwarfs]
+       
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(dwarfs):
+    return [f"{dwarf.capitalize()}!" for dwarf in dwarfs]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    cheese = ["cheddar", "gouda", "camembert"]
+    for snack in snacks:
+        if snack in cheese:
+            return snack
+
+
